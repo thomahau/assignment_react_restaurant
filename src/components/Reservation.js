c6c8e2afe1e3c31e9e90dd751daed0b3cbf117ea
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2018-01-22 20:05:22 +0100

    reservation form

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import data from '../data.js';
 
+const list = [
+  {
+    slot: '18:00',
+    size: '1 person'
+  },
+  {
+    slot: '18:30',
+    size: '2 people'
+  },
+  {
+    slot: '19:00',
+    size: '3 people'
+  },
+  {
+    slot: '19:30',
+    size: '4 people'
+  },
+  {
+    slot: '20:00',
+    size: '5 people'
+  },
+  {
+    slot: '20:30',
+    size: '6 people'
+  },
+  {
+    slot: '21:00',
+    size: '7 people'
+  },
+  {
+    slot: '21:30',
+    size: '8 people'
+  },
+  {
+    slot: '22:00',
+    size: '9 people'
+  }
+];
+
 const Reservation = () => {
   return (
     <div className="container" id="reservation">
@@ -9,7 +48,7 @@ const Reservation = () => {
         <div className="col-sm-5">
           <p>
             Book a table right here on the site, call us at{' '}
-            {data.reservation.phone}, or send us an email on{' '}
+            {data.reservation.phone},<br />or send us an email on{' '}
             <a href="mailto:joe@diner.com">joe@diner.com</a>.
           </p>
           <p>OPENING HOURS</p>
@@ -23,6 +62,37 @@ const Reservation = () => {
               </label>
               <div className="col-sm-9">
                 <input type="date" className="form-control" id="date" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label for="time" className="col-sm-3 control-label">
+                Time
+              </label>
+              <div className="col-sm-9">
+                <select className="form-control" id="time">
+                  {list.map(function(item) {
+                    return <option>{item.slot}</option>;
+                  })}
+                </select>
+              </div>
+            </div>
+            <div className="form-group">
+              <label for="diners" className="col-sm-3 control-label">
+                Diners
+              </label>
+              <div className="col-sm-9">
+                <select className="form-control" id="diners">
+                  {list.map(function(item) {
+                    return <option>{item.size}</option>;
+                  })}
+                </select>
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-offset-3 col-sm-9">
+                <button type="submit" className="btn btn-primary">
+                  Find a Table
+                </button>
               </div>
             </div>
           </form>
